73d6c4ca49dee05bcf5489b5b3d25e52b3212956
KJ Kim <genonfire@gmail.com>
2017-08-30 22:04:18 +0900

    [BBGO-17] Make navigator to inclusion_tag

---
+++ core/templatetags/navigator.py
@@ -0,0 +1,23 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('show_navigator.html', takes_context=True)
+def show_navigator(context):
+    """Show navigator"""
+    page = context['page']
+    index_begin = context['index_begin']
+    index_end = context['index_end']
+    index_total = context['index_total']
+    mindex_begin = context['mindex_begin']
+    mindex_end = context['mindex_end']
+
+    return {
+        'page': page,
+        'index_begin': index_begin,
+        'index_end': index_end,
+        'index_total': index_total,
+        'mindex_begin': mindex_begin,
+        'mindex_end': mindex_end,
+    }
