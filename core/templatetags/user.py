3cc03d7b562e65cd6bd32534e872b822ced663e6
KJ Kim <genonfire@gmail.com>
2017-08-30 23:13:09 +0900

    [BBGO-16] id_menu should be available only for members

--- core/templatetags/user.py
+++ core/templatetags/user.py
@@ -5,13 +5,15 @@ register = template.Library()
 
 
 @register.filter(name='nickname')
-def _nickname(user):
+def _nickname(user, is_authenticated=False):
     name = user
     if settings.ENABLE_NICKNAME and user.first_name:
         name = user.first_name
 
     if user.is_staff and not settings.DEBUG:
         return '<font color="#409BD1">%s</font>' % name
-    else:
+    elif is_authenticated:
         nametag = "<a href=javascript:void(0) onclick=\"javascript:id_menu(event, '%s');return false;\">%s</a>" % (user.username, name)
         return nametag
+    else:
+        return name
