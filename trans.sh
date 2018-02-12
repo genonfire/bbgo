f0c891e405788db24dd6761f51d221959e74b636
KJ Kim <genonfire@gmail.com>
2018-02-12 11:42:28 +0900

    update django-statici18n 1.7.0

--- trans.sh
+++ trans.sh
@@ -2,4 +2,4 @@ rm -rf static/jsi18n
 django-admin makemessages -a
 django-admin makemessages -d djangojs -a
 django-admin compilemessages
-python manage.py compilejsi18n -l ko-kr
+python manage.py compilejsi18n -l ko-KR
