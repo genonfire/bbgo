86489c030d774b6fd112ef281d37845fe31212e9
KJ Kim <gencode.me@gmail.com>
2019-07-22 22:30:47 +0900

    start bbgo 2.0

--- bbgo/urls.py
+++ bbgo/urls.py
@@ -1,20 +1,44 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-
-from rest_framework_swagger.views import get_swagger_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if 'rest_framework' in settings.INSTALLED_APPS:
-    urlpatterns += [path('restapi/', include('rest_framework.urls', namespace='rest_framework'))]
-
-if 'rest_framework_swagger' in settings.INSTALLED_APPS:
-    urlpatterns += [path('api/docs/', get_swagger_view(title='APIs'))]
 
 if 'rosetta' in settings.INSTALLED_APPS:
     urlpatterns += [path('trans/', include('rosetta.urls'))]
+
+if settings.LOCAL_SERVER:
+    if 'rest_framework' in settings.INSTALLED_APPS:
+            urlpatterns += [
+                path('restapi/', include(
+                    'rest_framework.urls', namespace='rest_framework'))
+            ]
+
+    if 'drf_yasg' in settings.INSTALLED_APPS:
+        from rest_framework import permissions
+        from drf_yasg.views import get_schema_view
+        from drf_yasg import openapi
+
+        schema_view = get_schema_view(
+            openapi.Info(
+                title="bbgo APIs",
+                default_version='beta',
+            ),
+            public=True,
+            permission_classes=(permissions.AllowAny,),
+        )
+
+        urlpatterns += [
+            url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(
+                cache_timeout=0), name='schema-json'),
+            url(r'^swagger/$', schema_view.with_ui(
+                'swagger', cache_timeout=0), name='schema-swagger-ui'),
+            url(r'^redoc/$', schema_view.with_ui(
+                'redoc', cache_timeout=0), name='schema-redoc'),
+        ]
--- runlocalserver.sh
+++ runlocalserver.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-find . -name \*.pyc -delete
 export DJANGO_DEBUG="Debug"
 python manage.py runserver
